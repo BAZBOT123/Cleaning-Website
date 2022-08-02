a7d6147d3fc888a6a1bba75ca5742299b44e0c44
BAZBOT123 <hughesy1993@hotmail.co.uk>
2022-08-02 22:22:12 +0100

    added logo and various components to Home Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faVacuum } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <nav className="navbar">
+      <div className="Logo">
+      <FontAwesomeIcon icon={faVacuum} />
+        </div>
+        <div className="Home-page">
+          <h1>Home</h1>
+        </div>
+        <div className="Reviews-page">
+          <h1>Customer Reviews</h1>
+        </div>
+        <div className="Services-page">
+          <h1>Services</h1>
+        </div>
+        <div className="About-us-page">
+          <h1>About Us</h1>
+        </div>
+        <div className="Contact-us-page">
+          <h1>Contact Us</h1>
+        </div>
+      </nav>
+
+      <main className="App-header">
+        <h1>Hello World!</h1>
+      </main>
+
+
+      <footer className="App-footer">
+        <h1>Contact number: 999</h1>
+      </footer>
     </div>
   );
 }
