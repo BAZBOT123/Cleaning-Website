cba8500c2faf9c6dd3acf48e769f9fb235de6190
BAZBOT123 <hughesy1993@hotmail.co.uk>
2022-08-09 12:38:56 +0100

    added contents in About page

--- src/components/About.js
+++ src/components/About.js
@@ -30,7 +30,22 @@ export default function About() {
       </nav>
 
       <main className="App-header">
-        <h1>Hiiii Hiiii!</h1>
+        <h3>So you want to know more about our cleaning?</h3>
+
+        <h4> You’re in the right place. Here, you’ll find everything you need to know about Lily's cleaning. Because it’s what we do best.</h4>
+
+
+        <h1>Our story</h1>
+        <h4>Born in South Africa with a blend of Portuguese and British culture. It all started in Mozambique, home of the hot sun, rich soil... 
+          and African Bird’s Eye Chillies. Also known as PERi-PERi. Small but mighty, these chillies have been used for centuries in local Mozambiquan cooking. 
+          The Portuguese then added a squeeze of lemon and kick of garlic to make the first PERi-PERi sauce. 
+          Used on just about anything, they found it was the ultimate baste for flame-grilling spatchcocked chicken. 
+          And it wasn’t long before two friends in Johannesburg came to experience the taste of PERi-PERi chicken and Nando’s was born.  
+          <br />
+          <br />
+Fast forward a few years, restaurants and Fino Pittas and today, the Nando’s PERi-PERi you know and love still pays homage to those roots. </h4>
+
+<h4>Reach out to us</h4>
       </main>
 
       <footer className="App-footer">
