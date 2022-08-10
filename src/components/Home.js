f4b94b824d5d616ab5799023d2184a04669900e4
BAZBOT123 <hughesy1993@hotmail.co.uk>
2022-08-10 13:14:19 +0100

    added responsiveness to SVG icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,8 +33,8 @@ export default function Home() {
 
       <main className="App-header">
       <div>
-      <Link to='https://react.semantic-ui.com/'>
-      <button type="button" className="btn btn-info">Button</button>
+      <Link to='/about'>
+      <button type="button" className="btn btn-info">Enter</button>
       </Link>
       </div>
       </main>
