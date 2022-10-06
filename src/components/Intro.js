b0a1f39d3b3157486af6ecde393cefe3596359a3
BAZBOT123 <hughesy1993@hotmail.co.uk>
2022-10-06 20:48:42 +0100

    added some styling to the intro page

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,17 +1,15 @@
 import '../components/intro.css';
 import { Link } from "react-router-dom"
-import React, { Component } from 'react';
-
+import React from 'react';
 
 export default function Intro() {
     return (
-
-        <main className="App-header">
-        <div>
-      <Link to='/about'>
-      <button type="button" className="btn btn-info">Enter</button>
-      </Link>
-      </div>
-      </main>
+        <main className="intro-main">
+            <div>
+                <Link to='/home'>
+                    <button type="button" className="btn btn-info">Enter</button>
+                </Link>
+            </div>
+        </main>
     );
 }
