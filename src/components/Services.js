11a2251b4fc73f75987a87e65d3d4786d0f6b320
BAZBOT123 <hughesy1993@hotmail.co.uk>
2022-11-24 12:06:34 +0000

    Update to styling for Services page

--- src/components/Services.js
+++ src/components/Services.js
@@ -39,9 +39,9 @@ export default function Services() {
                 </p>
                 <div>
                     <h2>Commercial cleaning</h2>
-                    <div class="container three-column-grid">
-                        <div class="box">HOUSE CLEANING</div>
-                        <div class="box">HOUSE CLEANING<br />
+                    <div class="container two-column-grid">
+                        <div class="box-image">HOUSE CLEANING</div>
+                        <div class="box-text">HOUSE CLEANING<br />
                             We are aware that every house is different and has certain needs and routines.
                             All of Lily's staff are experienced and well trained to clean your house the best way,
                             taking into consideration your surroundings and lifestyle, to make you feel as if you have cleaned yourself,
@@ -49,11 +49,11 @@ export default function Services() {
                             That kind of trust is reserved only for family and friends. Here at Lily's Cleaners, we take your safety and peace of mind very seriously.
                             That’s why all of our maids are background and reference checked and bonded also.
                             You can book your slot of service easily, just with a simple call. A customer service attendant will be available to hear your requirements for long term or just for a one off service.
-                           You can arrange for a single slot cleaning or a recurring service for a essential price.
+                            You can arrange for a single slot cleaning or a recurring service for a essential price.
                             (you can pick weekly, bi-weekly, or monthly packages).</div>
 
-                        <div class="box">OFFICE CLEANING</div>
-                        <div class="box">OFFICE CLEANING<br />
+                        <div class="box-image">OFFICE CLEANING</div>
+                        <div class="box-text">OFFICE CLEANING<br />
                             You can take care of the  business, and we’ll take care of the cleaning. When having an office,
                             you would need to sustain having it well-organised and clean for employees or clients.
                             Whenever the tables may need a good polish, the floors need a good vaccum, or the office needs freshened up,
@@ -61,11 +61,11 @@ export default function Services() {
                             highly trained, and at the ready to keep your facilities in the nick of shape. And not to worry, all of our cleaners are background checked,
                             and bonded, so rest assured everything will be in up to the highest standard and secure.
                             You can book your slot of service easily, just with a simple call. A customer service attendant will be available to hear your requirements for long term or just for a one off service.
-                           You can arrange for a single slot cleaning or a recurring service for a essential price.
+                            You can arrange for a single slot cleaning or a recurring service for a essential price.
                             (you can pick weekly, bi-weekly, or monthly packages)</div>
 
-                        <div class="box">VACATION RENTAL CLEANING</div>
-                        <div class="box">VACATION RENTAL CLEANING<br />
+                        <div class="box-image">VACATION RENTAL CLEANING</div>
+                        <div class="box-text">VACATION RENTAL CLEANING<br />
                             We want your rental property to be so fresh and clean for you to show it to your future guests proudly.
                             Whether you are a local or national company or just a regular homeowner, our team of maids can efficiently deliver the job.
                             We provide only high-quality service while staying within budget, and during the time frame that you requested.
@@ -75,8 +75,8 @@ export default function Services() {
                             flexibility and convenience that only a local company can provide.
                             Book today your cleaning service through our fast and secure calling system.</div>
 
-                        <div class="box">DEEP CLEANING</div>
-                        <div class="box">DEEP CLEANING<br />
+                        <div class="box-image">DEEP CLEANING</div>
+                        <div class="box-text">DEEP CLEANING<br />
                             A deep cleaning service is recommended for houses and offices that haven’t been cleaned in a while,
                             for first visit cleanings, or before and after an event.
                             Practically, a deep clean is needed whenever there’s been a lack of a professional clean that’s lasted weeks or even months.
@@ -86,8 +86,8 @@ export default function Services() {
                             Of course, we only sent to your home or office highly rated maids, background and reference checked,
                             so you don’t have to worry about letting us in at your place. Also, you can book easily by contacting us directly by a simple call.</div>
 
-                        <div class="box">MOVE-IN/MOVE-OUT CLEANING</div>
-                        <div class="box">MOVE-IN/MOVE-OUT CLEANING<br />
+                        <div class="box-image">MOVE-IN/MOVE-OUT CLEANING</div>
+                        <div class="box-text">MOVE-IN/MOVE-OUT CLEANING<br />
                             When you are moving to a new home, you probably are too busy for that last deep clean to your old house.
                             Or perhaps it’s your new home that needs cleaning? Whatever the case, our maids are ready to help you with this big step in your life.
                             We know that every move is different. Every house has it’s details that need specific attention, and a history that indicates where to clean the most.
@@ -95,24 +95,24 @@ export default function Services() {
                             You tell us your personal goals, and we’ll make you an estimated budget. Choose your cleaning tasks in our extensive checklist, and we’ll follow just as you request.
                             All of our staff are experienced and well-trained to accomplish this job. And also, you can book easily by contacting us directly by a simple call.</div>
 
-                        <div class="box">POST CONSTRUCTION CLEANING</div>
-                        <div class="box">POST CONSTRUCTION CLEANING <br />
+                        <div class="box-image">POST CONSTRUCTION CLEANING</div>
+                        <div class="box-text">POST CONSTRUCTION CLEANING <br />
                             This service usually is for commercial clients who are in the real state business or may have a new renovated home, build, or apartment,
                             but we provide post-construction cleaning to whichever homeowner that just have finished its remodeling or construction.
                             We must remove all the dust, dirt, and debris left behind from your new home or office. This is a job for an experienced and well-trained cleaner,
                             like our cleaners, who know the process of eliminating the particles of construction materials and know how to use the right equipment.
                             We assure you a high-quality job and a clean and smooth result. Also, we can include and schedule a complimentary cleaning session to your service
-                            to guarantee the elimination of all debris and dust. Ask for our complete cleaning checklist with all the possible spots and areas to clean. 
+                            to guarantee the elimination of all debris and dust. Ask for our complete cleaning checklist with all the possible spots and areas to clean.
                             Book easily by contacting us directly by a simple call.</div>
 
-                        <div class="box">APARTMENT CLEANING</div>
-                        <div class="box">APARTMENT CLEANING <br />
+                        <div class="box-image">APARTMENT CLEANING</div>
+                        <div class="box-text">APARTMENT CLEANING <br />
                             Either you want a frequent service with weekly or monthly scheduled sessions, or you just want a one-time cleaning with affordable hourly rates,
                             you better be sure that Lily's Cleaners have the right service for you. We strive to always fit under our customer’s budget and adjust to their time frames.
                             Also, we take your safety and satisfaction very seriously; that’s why every one of our cleaners went through an extensive selection process,
                             where we check out their experience, profile, and background references. In addition to that, we insure and bond all of our maids, for you not to worry about anything.
                             Booking our apartment cleaning service means that you will have a five-star cleaning, a helpful high rated cleaner in your home,
-                            and also you’ll be assigned to a project manager that will give proper tracing to your service. 
+                            and also you’ll be assigned to a project manager that will give proper tracing to your service.
                             Book our cleaning service by contacting us directly by a simple call.</div>
                     </div>
                 </div>
