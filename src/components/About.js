dc81ecfe1a85ae4e129dc288d1579c26744dfb29
BAZBOT123 <hughesy1993@hotmail.co.uk>
2022-12-01 21:13:30 +0000

    Added an image in the About page

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ export default function About() {
     <div className="App">
       <nav className="navbar">
         <div className="logo-lily">
-          <img src={Logo} alt="Girl in a jacket" height="45"></img>
+          <img src={Logo} alt="Lily's Cleaners logo" height="45"></img>
         </div>
         <div className="Home-page">
           <h1 className='home-font'><Link to='/home'>Home</Link></h1>
@@ -32,30 +32,26 @@ export default function About() {
       </nav>
 
       <main className="App-header">
-        <h3>So you want to know more about our cleaning?</h3>
-
+        <div className="logo-lily">
+          <img src={Logo} alt="Lily's Cleaners logo" height="80"></img>
+        </div>
+        <p>So you want to know more about our cleaning?</p>
         <p> You’re in the right place. Here, you’ll find everything you need to know about Lily's Cleaners. Because it’s what we do best.</p>
-
         <br />
-
         <h1>Lily's Cleaners story</h1>
         <p>Our story began in 2004 in <a href="https://en.wikipedia.org/wiki/Newcastle_upon_Tyne"
         >Newcastle Upon Tyne</a>, where we looked around and realised that there wasn't much domestic cleaning services around various areas being advertised, so this is where Lily's Cleaners stepped in.
           Through the years, we have relocated into London where we offer services to high valued clientele that we have gained great ongoing relations with.
         </p>
-
         <h1>Our Mission</h1>
         <p>We have an ethos to provide sustainable, fulfilling careers for our cleaners.
           In an industry where plenty of unfair and dubious business practices (such as unpaid overtime & travel time and below minimum-wage compensation) are the norms, Lily's Cleaners instead operates on responsible business practices.
           This means we employ our cleaners, affording them the rights and benefits that come with that, including paid travel time, paid overtime, a healthy working environment and so forth...
         </p>
-
         <p>
           Ultimately, we believe that our dual emphasis on customer experience and cleaner development is the reason why we have been able to deliver on hundreds of thousands of cleans and will always be the reason why we continue to clean, one home at a time.
         </p>
-
       </main>
-
       <footer className="App-footer">
         <h1>We make homes clean, safe, healthy.</h1>
         <h1 className='follow-us'>Follow us:  <a href="https://www.google.com/"><InstgramSvg /></a></h1>
